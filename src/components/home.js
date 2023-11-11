66c35b2283b594429f1b429e93a8e67d3f79cfc1
Anubhav <121851863+Anubhavprogramer@users.noreply.github.com>
2023-11-11 12:06:43 +0530

    updates

--- src/components/home.js
+++ src/components/home.js
@@ -1,24 +1,39 @@
-import React, { Component } from 'react'
-import logo from './newslogo.png'
+import React, { Component } from "react";
+import logo from "./newslogo.png";
+import react from "../assets/react.png";
+import bootstrap from "../assets/bootstrap.png";
+import Newsapi from "../assets/News API.png";
+import chatgpt from "../assets/ChatGPT_logo.svg.png";
+
 
 export default class home extends Component {
   render() {
     return (
-      <div className="container  text-center" style={{marginTop:'100px'}}>
-        <h1 >This is a new App made by me, Anubhav</h1>
-
-        <img src={logo} className="rounded mx-auto d-block mt-3" style={{width:'100px'}} alt="news Don"></img>
-        <h3 className="my-2">NEWS DON</h3>
-
-        <h4 className="my-5">LIST OF ITEMS I HAVE USED IN THIS PROJECT</h4>
-        <ul className="list-group list-group-flush">
-            <li className="list-group-item"><a href="https://react.dev/" className="link-primary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">React js</a></li>
-            <li className="list-group-item"><a href="https://getbootstrap.com/" className="link-primary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">Bootstrap</a></li>
-            <li className="list-group-item"><a href="https://newsapi.org/" className="link-primary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">For news-news api</a></li>
-            <li className="list-group-item"><a href="https://chat.openai.com/" className="link-primary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">For errors-Chat Gpt</a></li>
-            <li className="list-group-item"><a href="https://www.codewithharry.com/" className="link-primary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">Inspiration-code with harry</a></li>
-        </ul>
+      <div className="home_box">
+        <div className="branding">
+          <div className="colone">
+            <span className="textone">
+              NEWS
+            </span>
+              <br />
+            <span className="texttwo">
+              DOM
+            </span>
+          </div>
+          <div className="coltwo">
+              GEt the Lates and most accurate <br />
+              NEWS
+          </div>
+        </div>
+        <div className="socials">
+          <ul>
+            <li><a href="#"><img className="react" src={react} alt="" /></a></li>
+            <li><a href="#"><img className="bootstrap" src={bootstrap} alt="" /></a></li>
+            <li><a href="#"><img className="chatgpt" src={chatgpt} alt="" /></a></li>
+            <li><a href="#"><img className="newsapi" src={Newsapi} alt="" /></a></li>
+          </ul>
+        </div>
       </div>
-    )
+          );
   }
 }
